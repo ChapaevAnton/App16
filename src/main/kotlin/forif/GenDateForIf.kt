c0a603024e3680cf505399fe1203b4dc1b471cd4
ChapaevAnton <virsus2009@gmail.com>
2021-04-26 13:47:44 +0700

    16.4.5

--- src/main/kotlin/forif/GenDateForIf.kt
+++ src/main/kotlin/forif/GenDateForIf.kt
@@ -5,27 +5,36 @@ package forif
 fun main() {
 
 
-    var date: String? = ""
+    println(genDate("09.07.1984"))
+    println(genDate("01.01.2021"))
+    println(genDate("26.04.2021"))
 
-    for (i in 1..10) {
+
+}
+
+private fun genDate(strDate: String): String? {
+
+
+    val array: Array<String> = strDate.map { it.toString() }.toTypedArray()
+    var date = ""
+
+    for (i in 0..9) {
 
         when (date?.length) {
 
-            0 -> for (x in 0..3) if (x == 0) date += x
-            1 -> for (x in 0..9) if (x == 9) date += x
-            2 -> date += '.'
-            3 -> for (x in 0..1) if (x == 0) date += x
-            4 -> for (x in 0..9) if (x == 7) date += x
-            5 -> date += '.'
-            6 -> for (x in 0..9) if (x == 1) date += x
-            7 -> for (x in 0..9) if (x == 9) date += x
-            8 -> for (x in 0..9) if (x == 8) date += x
-            9 -> for (x in 0..9) if (x == 4) date += x
+            0 -> for (x in 0..3) if (x == array[i].toInt()) date += x
+            1 -> for (x in 0..9) if (x == array[i].toInt()) date += x
+            2 -> date += array[i]
+            3 -> for (x in 0..1) if (x == array[i].toInt()) date += x
+            4 -> for (x in 0..9) if (x == array[i].toInt()) date += x
+            5 -> date += array[i]
+            6 -> for (x in 0..9) if (x == array[i].toInt()) date += x
+            7 -> for (x in 0..9) if (x == array[i].toInt()) date += x
+            8 -> for (x in 0..9) if (x == array[i].toInt()) date += x
+            9 -> for (x in 0..9) if (x == array[i].toInt()) date += x
         }
 
     }
 
-    println(date)
-
-
+    return date
 }
